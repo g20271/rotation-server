facc4d041bf3a1188e77ec78725faa1780a4e279
g20271 <g20271@kagawa.kosen-ac.jp>
2023-07-07 02:00:46 +0900

    first commit

---
+++ Assets/Scripts/ExampleOscServer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleOscServer : MonoBehaviour
+{     
+   public void Hoge(float value) {
+       Debug.Log("Value: " + value);
+   }
+}
---
+++ Assets/Scripts/RotationInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationInput : MonoBehaviour
+{
+
+    Quaternion rot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rot = new Quaternion(0, 0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation = rot;
+    }
+
+    public void ReceiveW(float w) {
+        rot.w = w;
+    }
+    public void ReceiveX(float x) {
+        rot.x = x;
+    }
+    public void ReceiveY(float y) {
+        rot.y = y;
+    }
+    public void ReceiveZ(float z) {
+        rot.z = z;
+    }
+}
